d884323f4d79d99a91a50793c4f5d0fb2bb29046
Alanlin <871699990@qq.com>
2021-09-23 16:17:34 +0800

    new one

--- basic/point/point.go
+++ basic/point/point.go
@@ -4,15 +4,17 @@ import (
 	"github.com/Nik-U/pbc"
 	"github.com/ncw/gmp"
 )
+
 type point struct {
-	x	*gmp.Int
-	y	*gmp.Int
-	PolyWit	*pbc.Element
+	x       *gmp.Int
+	y       *gmp.Int
+	PolyWit *pbc.Element
 }
-func newPoint(x *gmp.Int,y *gmp.Int,w *pbc.Element)*point{
-return	&point{
-	x: x,
-	y: y,
-	PolyWit: w,
-}
+
+func NewPoint(x *gmp.Int, y *gmp.Int, w *pbc.Element) *point {
+	return &point{
+		x:       x,
+		y:       y,
+		PolyWit: w,
+	}
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
-module github.com/Alan-Lxc/crypto_contest/basic
+module github.com/Alan-Lxc/crypto_contest
+
+go 1.17
 
 require (
 	github.com/Nik-U/pbc v0.0.0-20181205041846-3e516ca0c5d6
-	github.com/ncw/gmp v1.0.3
+	github.com/ncw/gmp v1.0.4
 )
