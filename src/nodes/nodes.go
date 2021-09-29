03c277d4cdca49dcea9ebaf6f90e6912354fb369
Alan-Lxc <alanlin@buaa.edu.cn>
2021-09-29 02:28:17 -0700

    edit on 17:28 09/29

--- src/nodes/nodes.go
+++ src/nodes/nodes.go
@@ -2,9 +2,11 @@ package nodes
 
 import (
 	"errors"
+	"github.com/Alan-Lxc/crypto_contest/src/basic/interpolation"
 	"github.com/Alan-Lxc/crypto_contest/src/basic/point"
 	"github.com/Alan-Lxc/crypto_contest/src/basic/poly"
 	"github.com/ncw/gmp"
+	"log"
 	"math/rand"
 	"os"
 	"strconv"
@@ -13,26 +15,60 @@ import (
 
 type Node struct {
 	//Label of Node
-	Label int
+	label int
 	//Total number of Nodes
-	Total int
+	total int
 	//Degree of polynomial
-	Degree int
+	degree int
 	//the polynomial was set on Z_p
-	P *gmp.Int
+	p *gmp.Int
 	// Rand source
-	Randstate *rand.Rand
+	randstate *rand.Rand
 	//To store the point(shares) sent from other node
-	RecPoint []*point.Point
+	recPoint []*point.Point
 	//To recode the share that have already received
-	RecCounter int
+	recCounter int
 	//The poly reconstructed with the shares
-	RecPoly poly.Poly
+	recPoly poly.Poly
 }
 
+func (node Node) GetMsgFromNode(pointmsg point.Pointmsg) {
+	index := pointmsg.GetIndex()
+	log.Println("Phase 1 :[Node %d] receive point message from [Node %d]", node.label, index)
+	p := pointmsg.GetPoint()
+	//Receive the point and store
+	node.recPoint[node.recCounter] = p
+	node.recCounter += 1
+
+	if node.recCounter == node.total {
+		node.recCounter = 0
+		node.Phase1()
+	}
+}
+func (node Node) Phase1() {
+	log.Printf("[Node %d] now start phase1", node.label)
+	x_point := make([]*gmp.Int, node.degree+1)
+	y_point := make([]*gmp.Int, node.degree+1)
+	for i := 0; i <= node.degree; i++ {
+		x_point[i] = node.recPoint[i].X
+		y_point[i] = node.recPoint[i].Y
+	}
+	p, err := interpolation.LagrangeInterpolate(node.degree, x_point, y_point, node.p)
+	if err != nil {
+		for i := 0; i <= node.degree; i++ {
+			log.Print(x_point[i])
+			log.Print(y_point[i])
+		}
+		log.Print(err)
+		panic("Interpolation failed")
+	}
+	node.recPoly = p
+	log.Printf("Interpolation finished")
+	//node.Phase2()
+}
 func (node *Node) GetLabel() int {
 	if node != nil {
-		return node.Label
+		return node.label
 	} else {
 		return 0
 	}
@@ -49,16 +85,17 @@ func New(degree, label, counter int, logPath string) (Node, error) {
 	}
 	randState := rand.New(rand.NewSource(time.Now().Local().UnixNano()))
 	p := gmp.NewInt(0)
+	//Maybe We can generate a big prime?
 	p.SetString("57896044618658097711785492504343953926634992332820282019728792006155588075521", 10)
 
 	return Node{
-		Label:      label,
-		Total:      counter,
-		Degree:     degree,
-		P:          p,
-		Randstate:  randState,
-		RecPoint:   nil,
-		RecCounter: 0,
-		RecPoly:    poly.Poly{},
+		label:      label,
+		total:      counter,
+		degree:     degree,
+		p:          p,
+		randstate:  randState,
+		recPoint:   nil,
+		recCounter: 0,
+		recPoly:    poly.Poly{},
 	}, nil
 }
