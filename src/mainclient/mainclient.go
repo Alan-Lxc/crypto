5a1201cefd6c6647c0b8ee2c9fed681c99af8712
garyyyyyyy <1456180450@qq.com>
2021-10-10 10:48:52 +0800

    update

--- src/mainclient/mainclient.go
+++ src/mainclient/mainclient.go
@@ -22,6 +22,34 @@ func Initial(s0 string, degree int, counter int, metadataPath string) {
 	GeneratePoly(s0, degree, counter, metadataPath)
 }
 
+func GeneratePoly(s0 string, degree int, counter int, metadataPath string) {
+
+	//randState := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+	fixedRandState := rand.New(rand.NewSource(int64(3)))
+	p := gmp.NewInt(0)
+	p.SetString("57896044618658097711785492504343953926634992332820282019728792006155588075521", 10)
+	tmp := gmp.NewInt(0)
+	tmp.SetString(s0, 10)
+
+	polyy, _ := poly.NewRand(degree, fixedRandState, p)
+	polyy.SetCoeffWithGmp(0, tmp)
+
+	//nn := make([]nodes.Node, counter)
+	//????
+	for i := 0; i < counter; i++ {
+		x := int32(i)
+		y := gmp.NewInt(0)
+		poly.EvalMod(gmp.NewInt(int64(x)), p, y)
+
+		polytmp, _ := poly.NewRand(degree, fixedRandState, p)
+		polytmp.SetCoeffWithGmp(0, y)
+
+		n, _ := nodes.New(degree, i, counter, metadataPath, p)
+		n.Service()
+	}
+
+}
+
 func Handoff(metadataPath string) {
 
 	clock, _ := clock.New(metadataPath)
@@ -34,30 +62,6 @@ func GetAnswer(metadataPath string) string {
 	clock.Connect()
 	ans := clock.ClientGetAnswer()
 	return ans
-}
-
-func GeneratePoly(s0 string, degree int, counter int, metadataPath string) {
-
-	//randState := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
-	fixedRandState := rand.New(rand.NewSource(int64(3)))
-	p := gmp.NewInt(0)
-	p.SetString("57896044618658097711785492504343953926634992332820282019728792006155588075521", 10)
-	tmp := gmp.NewInt(0)
-	tmp.SetString(s0, 10)
-
-	poly, _ := poly.NewRand(degree, fixedRandState, p)
-	poly.SetCoeffWithGmp(0, tmp)
-
-	//nn := make([]nodes.Node, counter)
-	//????
-	for i := 0; i < counter; i++ {
-		x := int32(i)
-		y := gmp.NewInt(0)
-		poly.EvalMod(gmp.NewInt(int64(x)), p, y)
-		n, _ := nodes.New(degree, i, counter, metadataPath, p, y)
-		n.Service()
-	}
-
 }
 
 func Gets0() string {
