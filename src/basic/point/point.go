f6058f0ef31ca5f3d84a2ab90b578e55a2514270
garyyyyyyy <1456180450@qq.com>
2021-11-05 01:06:43 +0800

    update

--- src/basic/point/point.go
+++ src/basic/point/point.go
@@ -19,34 +19,34 @@ func NewPoint(x *gmp.Int, y *gmp.Int, w []*pbc.Element) *Point {
 	}
 }
 
-type Pointmsg struct {
-	index int
-	point *Point
-}
-
-func (pointmsg *Pointmsg) GetIndex() int {
-	if pointmsg != nil {
-		return pointmsg.index
-	} else {
-		return 0
-	}
-}
-func (pointmsg *Pointmsg) GetPoint() *Point {
-	if pointmsg != nil {
-		return pointmsg.point
-	} else {
-		return nil
-	}
-}
-func (pointmsg *Pointmsg) SetPoint(point *Point) {
-	if point != nil {
-		pointmsg.point = point
-	}
-}
-
-//transport a object value to a method must use pointer
-func (pointmsg *Pointmsg) SetIndex(index int) {
-	if index > 0 {
-		pointmsg.index = index
-	}
-}
+//type Pointmsg struct {
+//	index int
+//	point *Point
+//}
+//
+//func (pointmsg *Pointmsg) GetIndex() int {
+//	if pointmsg != nil {
+//		return pointmsg.index
+//	} else {
+//		return 0
+//	}
+//}
+//func (pointmsg *Pointmsg) GetPoint() *Point {
+//	if pointmsg != nil {
+//		return pointmsg.point
+//	} else {
+//		return nil
+//	}
+//}
+//func (pointmsg *Pointmsg) SetPoint(point *Point) {
+//	if point != nil {
+//		pointmsg.point = point
+//	}
+//}
+//
+////transport a object value to a method must use pointer
+//func (pointmsg *Pointmsg) SetIndex(index int) {
+//	if index > 0 {
+//		pointmsg.index = index
+//	}
+//}
